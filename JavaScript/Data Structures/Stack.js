fdf6701a4b16017bb9a66e04ca5bfce9e2bde768
Jose Martinez <josmmartinez94@gmail.com>
2018-11-26 21:36:27 -0800

    Restructure directories

--- JavaScript/Data Structures/Stack.js
+++ JavaScript/Data Structures/Stack.js
@@ -114,5 +114,31 @@ let Stack = (function () {
   // will return an instance of the Stack class
 })();
 
-let stack = new Stack();
-console.log(stack.isEmpty());
+// Stacks can solve a variety of problems, including:
+// backtracking problems to remember tasks or paths visited, and to undo actions
+
+
+// Decimal to Binary
+// convert a decimal number to a binary representation
+
+// Approach:
+// divide the number by 2 until the division result is 0.
+function divideBy2(decNumber) {
+  var remStack = new Stack(), rem, binaryString = "";
+
+  // while the division result is not zero
+  // we get the remainder of the divisq and push it to the stack
+  // finally we update the number that will be divided by 2
+  while (decNumber > 0) {
+    rem = Math.floor(decNumber % 2);
+    remStack.push(rem);
+    decNumber = Math.floor(decNumber / 2);
+  }
+
+  /// we then pop elements from the stack  until it is empty
+  while (!remStack.isEmpty()) {
+    binaryString += remStack.pop().toString();
+  }
+
+  return binaryString;
+}
