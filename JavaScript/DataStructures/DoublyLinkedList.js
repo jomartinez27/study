fde9542b39823f9edbc1e6d71f477d72efd27004
Jose Martinez <josmmartinez94@gmail.com>
2018-12-01 18:14:39 -0800

    Add insertAt doubly  linkedlist es6

--- JavaScript/DataStructures/DoublyLinkedList.js
+++ JavaScript/DataStructures/DoublyLinkedList.js
@@ -120,4 +120,37 @@ export default class DoublyLinkedList {
     }
     this.count++;
   }
+
+  insertAt(element, index) {
+    if (index > -1 && index <= this.count) {
+      let node = new Node(element);
+      let current = this.head;
+      if (index === 0) {
+        if (this.head === null) {
+          this.head = node;
+          this.tail = node;
+        } else {
+          node.next = this.head;
+          this.head.prev = node;
+          this.head = node;
+        }
+      } else if (index === this.count) {
+        current = this.tail;
+        current.next = node;
+        node.prev = current;
+        this.tail = node;
+      } else {
+        const previous = this.getElementAt(index - 1);
+        current = previous.next;
+        node.next = current;
+        previous.next = node;
+        current.prev = node;
+        node.prev = previous;
+      }
+      this.count++;
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
