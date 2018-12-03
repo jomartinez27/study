c697a8fdc7d259fbe0f66f1b7397393c65656bcb
Jose Martinez <josmmartinez94@gmail.com>
2018-12-02 21:36:45 -0800

    Add skeleton es5 set class

---
+++ JavaScript/DataStructures/Set.js
@@ -0,0 +1,40 @@
+// set is a sequential data structure that does not allow duplicated values
+// A set is a collection of items that are not unordered and consits of
+// unique elements. In math, a set is a collection of distinct objects
+// ex we have a set of natural numbers, consisting of integer numbers
+// greater than or equal to 0
+
+// es5 implementation
+function Set() {
+  let items = {};
+
+  // add(val): adds a new item to the set
+  this.add = function(value) {
+
+  }
+
+  //delete(value): removes the value from the set
+  this.delete = function(value) {
+
+  }
+
+  //has(value): returns true if the value exists in the set and false otherwise
+  this.has = function(value) {
+
+  }
+
+  //clear(): removes all the items from the set
+  this.clear = function() {
+
+  }
+
+  //size(): returns the length of items
+  this.size = function() {
+
+  }
+
+  //values(): returns an array of all the values of the set
+  this.values = function() {
+    
+  }
+}
