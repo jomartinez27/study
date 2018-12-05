470ae0d4781a80ec8121b14224a3943e898527d3
Jose Martinez <josmmartinez94@gmail.com>
2018-12-04 17:30:31 -0800

    Add skeleton es5 Dictionary class

--- JavaScript/DataStructures/Dictionary.js
+++ JavaScript/DataStructures/Dictionary.js
@@ -3,3 +3,46 @@
 // element. A dictionary is also known as a "map".
 
 //es5 implementation
+function Dictionary() {
+  var item = {};
+
+  //set(key, value): adds a new item to the dictionary
+  this.set = function(key, value) {
+
+  }
+
+  //delete(key): removes the value from the dictionary using the key
+  this.delete = function(key) {
+
+  }
+
+  //has(key): returns true if the key exists in the dictionary and false otherwise
+  this.has(key) = function(key) {
+
+  }
+
+  //get(key): returns a specific value searched by the key
+  this.get(key) = function(key) {
+
+  }
+
+  //clear(): removes all the items from the dictionary
+  this.clear = function() {
+
+  }
+
+  //size(): returns how many elements the dictionary contains
+  this.size = function() {
+
+  }
+
+  //keys(): returns an array of all the keys in the dictionary
+  this.keys = function() {
+
+  }
+
+  //values(): returns an array of all the values of the dictionary
+  this.values = function() {
+    
+  }
+}
