8ef969a6a8ac2048fa4d95eef9b573058f5868a7
Jose Martinez <josmmartinez94@gmail.com>
2018-12-04 17:55:45 -0800

    Add skeleton hash table es5 class

---
+++ JavaScript/DataStructures/Hash.js
@@ -0,0 +1,23 @@
+// Hashing consists of finding a vlaue in a data structure in the shortest
+// time possible. A hash function is a function that, given a key, will return
+// an address in the table where the value is
+
+//es5
+function HashTable() {
+  var table = [];
+
+  //put(key, value): adds a new item to the hash table
+  this.put = function(key, value) {
+
+  }
+
+  //remove(key): removes the value from the hash table using the key
+  this.remove = function(key) {
+
+  }
+
+  //get(key): returns a specific value searched by the key
+  this.get = function(key) {
+    
+  }
+}
