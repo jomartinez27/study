cf287d939802a595a141717dfe4d377d7d596dde
Jose Martinez <josmmartinez94@gmail.com>
2018-12-04 17:25:04 -0800

    Add context for dictionary, start es5 implementation

---
+++ JavaScript/DataStructures/Dictionary.js
@@ -0,0 +1,5 @@
+// Sets, dictionaries, and hashes store unique values. A dictionary is
+// used to store [key,value] pairs, where the key is used to find a particular
+// element. A dictionary is also known as a "map".
+
+//es5 implementation
