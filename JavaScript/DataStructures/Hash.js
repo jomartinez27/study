40a8d25cf8ef3ec296f6d5b77a902480cf6992e0
Jose Martinez <josmmartinez94@gmail.com>
2018-12-07 12:19:35 -0800

    Fix type toString es6 HashTable method

--- JavaScript/DataStructures/Hash.js
+++ JavaScript/DataStructures/Hash.js
@@ -227,9 +227,9 @@ class HashTable {
       return '';
     }
     const keys = Object.keys(this.table);
-    let objString = `${keys[0]} => ${this.table[keys[0]].toString()}`;
+    let objString = `{${keys[0]} => ${this.table[keys[0]].toString()}}`;
     for (let i = 1; i < keys.length; i++) {
-      objString = `${objString},${keys[i]} => ${this.table[keys[i]].toString()}`;
+      objString = `${objString},{${keys[i]} => ${this.table[keys[i]].toString()}}`;
     }
     return objString;
   }
