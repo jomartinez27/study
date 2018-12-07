e5f57d888a470b4697b0ad3eac9096b294625efb
Jose Martinez <josmmartinez94@gmail.com>
2018-12-07 12:28:37 -0800

    Add description of tree, binary tree, and binary search tree

---
+++ JavaScript/DataStructures/Tree.js
@@ -0,0 +1,21 @@
+// A tree is an abstract model of a hierarchical structure. The most common
+// example of a tree in real life would be a family tree or a company
+// organizational chart
+
+// A tree consists of nodes with a parent-child relationship. The top node
+// of a tree is called the root. Each element of the tree is called a node
+// There are "internal nodes" and "external nodes".
+// Internal nodes - is a node with at least one child
+// External nodes - is a node that does not have children
+
+// A node can have ancestors and descendants. The ancestors (except for root)
+// are parent, grandparent, great-grandparent and so on.
+// A subtree is a tree inside another tree. The depth of a node consits of the
+// number of ancestors. The height of a tree consists of the maximum depth of any node
+
+// Binary Tree - A node in a Binary tree has two at most two children, one left and one right
+// A Binary Search Tree - is a Binary Tree, but it only allows ypu to store
+// nodes with lesser values on the left-hand side and nodes with greater values on the right
+// hand side.
+
+//es5 Binary Search Tree
