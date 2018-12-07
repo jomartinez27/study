d4ade98036ef3789715c9b5831c14acb78ee9c30
Jose Martinez <josmmartinez94@gmail.com>
2018-12-07 12:11:23 -0800

    Add remove method es6 HashTable

--- JavaScript/DataStructures/Hash.js
+++ JavaScript/DataStructures/Hash.js
@@ -195,4 +195,14 @@ class HashTable {
     const valuePair = this.table[this.hashCode(key)];
     return valuePair == null ? undefined : valuePair.value;
   }
+
+  remove(key) {
+    const hash = this.hashCode(key);
+    const valuePair = this.table[hash];
+    if (valuePair != null) {
+      delete this.table[hash];
+      return true;
+    }
+    return false;
+  }
 }
