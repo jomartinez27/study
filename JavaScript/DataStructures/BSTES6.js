248a1e3508ed9e66498f2d2503d0f0b29907000e
Jose Martinez <josmmartinez94@gmail.com>
2018-12-12 11:50:21 -0800

    Add node class, start insert method es6 bst

--- JavaScript/DataStructures/BSTES6.js
+++ JavaScript/DataStructures/BSTES6.js
@@ -1,6 +1,20 @@
 // es6 bst
+class Node {
+  constructor(key) {
+    this.key = key;
+    this.left = null;
+    this.right = null;
+  }
+}
+
 class BinaryTree {
   constructor() {
     this.root = null;
+  }
+
+  insert(key) {
+    if (this.root === null) {
+      this.root = new Node(key;
+    }
   }
 }
