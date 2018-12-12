52f963eb968484f9fabbc6a73ed095a95d814492
Jose Martinez <josmmartinez94@gmail.com>
2018-12-12 12:03:14 -0800

    Add insertNode method es6, fix typo, add getRoot method es6 bst

--- JavaScript/DataStructures/BSTES6.js
+++ JavaScript/DataStructures/BSTES6.js
@@ -14,7 +14,34 @@ class BinaryTree {
 
   insert(key) {
     if (this.root === null) {
-      this.root = new Node(key;
+      this.root = new Node(key);
+    } else {
+      this.insertNode(this.root, key);
     }
+  }
+
+  insertNode(node, key) {
+    let newNode = new Node(key);
+    if (newNode.key < node.key) {
+      if (node.left === null) {
+        node.lefft = newNode;
+      } else {
+        insertNode(node.left, newNode);
+      }
+    } else {
+      if (node.right === null) {
+        node.right = newNode;
+      } else {
+        insertNode(node.right, newNode)
+      }
+    }
+  }
+
+  getRoot() {
+    return this.root;
+  }
+
+  searchNode() {
+    
   }
 }
