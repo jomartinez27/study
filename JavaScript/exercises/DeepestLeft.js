63417d74051ab1de4d15426213e792d31bf1c120
Jose Martinez <josmmartinez94@gmail.com>
2018-12-16 20:44:30 -0800

    Test answer deepestLeftMost

--- JavaScript/exercises/DeepestLeft.js
+++ JavaScript/exercises/DeepestLeft.js
@@ -4,6 +4,18 @@
 // Approach: We can perform BreadthFirstSearch to get the deepest node,
 // and reverse BreadthFirstSearch to get the left most, deepest node
 
-const deepestLeft = (root) {
-
+const deepestLeft = (root) => {
+  let queue = [root];
+  let leftMost;
+  while (queue.length > 0) {
+    let current = queue.shift();
+    if (current.right) {
+      queue.unshift(current.right);
+    }
+    if (current.left) {
+      queue.unshift(current.left);
+    }
+    leftMost = current;
+  }
+  return leftMost;
 }
