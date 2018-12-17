18981327d8174e3de32fd29b3722fd875c8df1f7
Jose Martinez <josmmartinez94@gmail.com>
2018-12-16 20:01:26 -0800

    Add approach for inorder traversal question

--- JavaScript/exercises/InorderTraversal.js
+++ JavaScript/exercises/InorderTraversal.js
@@ -6,3 +6,7 @@
 //  /
 // 3
 // return [1,3,2]
+
+// Approach: We need to use a Stack to store all the root elements so that
+// the left subtrees can be processed. Once the left subtree is processed,
+// the root value is added to the list and process is repeated for the right
