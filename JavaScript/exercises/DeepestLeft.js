f9c5e65585476b0c1a6ec70de75250378b71d41e
Jose Martinez <josmmartinez94@gmail.com>
2018-12-16 20:15:04 -0800

    Start answer, fix typo

--- JavaScript/exercises/DeepestLeft.js
+++ JavaScript/exercises/DeepestLeft.js
@@ -1,5 +1,9 @@
-// Deepest Left-Most Node: Given a tree, return the deepest left-most
+// Deepest Left-Most Node: Given a root node, return the deepest left-most
 //node in the tree. You may assume that there will always be a valid tree
 
 // Approach: We can perform BreadthFirstSearch to get the deepest node,
 // and reverse BreadthFirstSearch to get the left most, deepest node
+
+const deepestLeft = (root) {
+
+}
