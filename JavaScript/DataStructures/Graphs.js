c5ff32fd968a66663facd05dac2933d79f5aa9f3
Jose Martinez <josmmartinez94@gmail.com>
2018-12-18 16:29:15 -0800

    Add description and diagram of graph Graph.js

---
+++ JavaScript/DataStructures/Graphs.js
@@ -0,0 +1,19 @@
+// A Graph is an abstract model of a network structure. A graph is a set
+// of nodes (or vertices) connected by edges. Learning about graphs is important
+// because any binary relationship can be represented by a graph
+
+// Any social network, Facebook, Twitter etc. can be represented by a graph
+// We can also use graphs to represent roads, flights, and communications
+
+// A graph is represented as G = (V,E)
+// V: A set of vertices
+// E: A set of edges connection the vertices in V
+
+// Diagram of a graph
+//                A
+//              /  | \
+//            B    C - D
+//           / \    \  / \
+//          E   F    G    H
+//          |
+//          I
