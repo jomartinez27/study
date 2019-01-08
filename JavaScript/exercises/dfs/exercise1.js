031cf9d78f12f104be5f2f2cbd156e0aa68cdfd1
Jose Martinez <josmmartinez94@gmail.com>
2019-01-07 17:55:01 -0800

    Add description dfs, exercise1.js

--- JavaScript/exercises/dfs/exercise1.js
+++ JavaScript/exercises/dfs/exercise1.js
@@ -23,6 +23,15 @@ class Tree {
     // to check for a value inside a tree we will begin with declaring
     // a collection array, which contains the root node. The we build a
     // while loop that will run until there is no longer a value inside of the array
+
+    // DFS uses a Stack to traverse down the tree of nodes. We will declare the
+    // current node by shifting off the first value of the array. With this
+    // node, we will check to see if its data is equal to the value we are searching
+    //for. If it is equal, we will return true and exit, if the node's value does
+    // not match, we will push the children of that node to the front of the array
+    // if it exists. We unshift the children to the front because the DFS
+    // approach wants us to go all the way to the bottom of the tree before checking
+    // any sibling element. If no value matches after searching we return false
     let collection = [this.root];
 
     while (collection.length) {
