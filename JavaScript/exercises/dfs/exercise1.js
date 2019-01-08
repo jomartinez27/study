72ad761522dd54a8e0440cf617f92cda20a61f74
Jose Martinez <josmmartinez94@gmail.com>
2019-01-07 17:44:07 -0800

    Add Node class for tree

--- JavaScript/exercises/dfs/exercise1.js
+++ JavaScript/exercises/dfs/exercise1.js
@@ -3,4 +3,12 @@ class Node {
     this.value = value;
     this.children = [];
   }
+
+  add(value) {
+    this.children.push(new Node(value));
+  }
+
+  remove(value) {
+    this.children = this.children.filter(child => child.value !== value);
+  }
 }
