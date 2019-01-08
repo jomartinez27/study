3de8af0c30e169d0fefc1b1757f7ce1aa004ddeb
Jose Martinez <josmmartinez94@gmail.com>
2019-01-07 17:51:23 -0800

    Add dfs for exercise1.js

--- JavaScript/exercises/dfs/exercise1.js
+++ JavaScript/exercises/dfs/exercise1.js
@@ -12,3 +12,28 @@ class Node {
     this.children = this.children.filter(child => child.value !== value);
   }
 }
+
+class Tree {
+  constructor() {
+    this.root = null;
+  }
+
+  //DFS to check for a value
+  depthFirstSearch(value) {
+    // to check for a value inside a tree we will begin with declaring
+    // a collection array, which contains the root node. The we build a
+    // while loop that will run until there is no longer a value inside of the array
+    let collection = [this.root];
+
+    while (collection.length) {
+      let node = collection.shift();
+
+      if (node.value === value) {
+        return true
+      } else {
+        collection.unshift(...node.children)
+      }
+    }
+    return false;
+  }
+}
